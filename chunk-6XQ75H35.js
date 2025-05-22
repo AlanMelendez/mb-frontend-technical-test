54ae44f1143440f2b4bbc066598811a774232b97
Alan Cuevas Melendez <73464077+AlanMelendez@users.noreply.github.com>
2025-05-22 14:02:03 -0600

    Auto-generated commit

---
+++ chunk-6XQ75H35.js
@@ -0,0 +1,1 @@
+var t={production:!1,internalAuth:{apiUrl:"https://prueba.sandboxmb.com/api"},externalAuth:{apiUrl:"https://prueba.sandboxmb.com/api"}};export{t as a};
---
+++ chunk-WFBH4SKI.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-WF4NN5EV.js";import"./chunk-NEUCA32X.js";export{a as ProductsHorizontalListComponent};
